3e3f7a6a876004f8406451d1558184545d74bbdc
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2021-08-31 15:36:48 -0300

    Hilt module

--- app/src/main/java/com/proway/resumo_curso/di/HiltModule.kt
+++ app/src/main/java/com/proway/resumo_curso/di/HiltModule.kt
@@ -1,9 +1,15 @@
 package com.proway.resumo_curso.di
 
+import android.content.Context
+import com.proway.resumo_curso.database.AppDatabase
+import com.proway.resumo_curso.database.dao.GithubDao
 import com.proway.resumo_curso.repository.GithubRepository
+import com.proway.resumo_curso.service.GithubServices
+import com.proway.resumo_curso.service.RetrofitService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -11,6 +17,14 @@ import dagger.hilt.components.SingletonComponent
 object HiltModule {
 
     @Provides
-    fun provideGithubRepository() : GithubRepository = GithubRepository()
+    fun provideGithubRepository(dao: GithubDao, services: GithubServices): GithubRepository = GithubRepository(dao, services)
+
+    @Provides
+    fun provideGithuServices(): GithubServices = RetrofitService.getGithubServices()
+
+    @Provides
+    fun provideGithubDao(@ApplicationContext context: Context): GithubDao {
+        return AppDatabase.getDatabase(context).getGithubDao()
+    }
 
 }
--- app/src/main/java/com/proway/resumo_curso/repository/GithubRepository.kt
+++ app/src/main/java/com/proway/resumo_curso/repository/GithubRepository.kt
@@ -1,21 +1,22 @@
 package com.proway.resumo_curso.repository
 
+import com.proway.resumo_curso.database.dao.GithubDao
 import com.proway.resumo_curso.model.GithubRepositoryResponse
+import com.proway.resumo_curso.service.GithubServices
 import com.proway.resumo_curso.service.RetrofitService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import javax.inject.Inject
 
 /**
  * Repositorio, será responsavel na interaçao com os dados que vem do Server/DAO/Firebase etc.
  * Netsa camada é onde criamos todos as funcoes precisam buscar dados, independente da sua fonte.
  */
-class GithubRepository() {
-
-    /**
-     * Buscamos nossa Interface implementada do retrofit
-     */
-    private val githubServices = RetrofitService.getGithubServices()
+class GithubRepository @Inject constructor(
+    private val githubDao: GithubDao,
+    private val githubServices : GithubServices
+) {
 
     /**
      * Vamos expor o serviço de fetchRepositories para as outras camadas.
